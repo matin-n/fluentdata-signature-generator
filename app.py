54d3a45c627ec2b663406e2fd7d7b7fa25590dd0
matin-n <40713017+matin-n@users.noreply.github.com>
2024-08-07 15:45:28 -0700

    Include phone number in signature

--- app.py
+++ app.py
@@ -8,14 +8,19 @@ st.set_page_config(
 )
 
 
-def create_email_signature(name, role, name_font_size=70, role_font_size=40):
+def create_email_signature(name, role, phone, name_font_size=70, role_font_size=40, phone_font_size=40):
     image = Image.open("signature.png")
     draw = ImageDraw.Draw(image)
-    font_bold = ImageFont.truetype("./font/Poppins-Bold.ttf", name_font_size)
-    font_regular = ImageFont.truetype("./font/Poppins-Regular.ttf", role_font_size)
+    font_name = ImageFont.truetype("./font/Poppins-Bold.ttf", name_font_size)
+    font_role = ImageFont.truetype("./font/Poppins-Italic.ttf", role_font_size)
+    font_phone = ImageFont.truetype("./font/Poppins-Regular.ttf", phone_font_size)
 
-    draw.text((972.6, 119), name, font=font_bold, fill="white")
-    draw.text((972.6, 215), role, font=font_regular, fill="white")
+    # Name
+    draw.text((975.2, 63.9), name, font=font_name, fill="white")
+    # Role
+    draw.text((972.6, 170), role, font=font_role, fill="white")
+    # Phone
+    draw.text((1068.1, 257.8), text=phone, font=font_phone, fill="white")
 
     return image
 
@@ -28,12 +33,14 @@ sidebar = st.sidebar
 
 name = signature_form.text_input("Your name")
 role = signature_form.text_input("Your role")
+phone = signature_form.text_input("Your phone number")
 
 # Font settings
 sidebar.title("Font settings")
 # TODO: Changing the sidebar values refreshes the page, which is not ideal
 name_font_size = sidebar.slider("Name font size", 0, 100, 70)
 role_font_size = sidebar.slider("Role font size", 0, 100, 40)
+phone_font_size = sidebar.slider("Phone font size", 0, 100, 40)
 
 submit = signature_form.form_submit_button(label="Create email signature")
 
@@ -41,7 +48,7 @@ if submit:
     st.markdown("### Download your signature")
     st.markdown("Click the button below to download your email signature")
 
-    signature = create_email_signature(name, role, name_font_size, role_font_size)
+    signature = create_email_signature(name, role, phone, name_font_size, role_font_size, phone_font_size)
     st.image(signature)
 
     # TODO: Streamlit doesn't accept this?
